bcb199e3223ac916faf257ae02a87a3f6aa159e0
pareshrajput <garvitinfo.paresh@gmail.com>
2025-02-10 08:35:39 +0530

    10-02-25

---
+++ thread4.java
@@ -0,0 +1,20 @@
+class thread4 extends Thread {
+
+  public void run() {
+    System.out.println(
+      "running thread name is:" + Thread.currentThread().getName()
+    );
+    System.out.println(
+      "running thread priority is:" + Thread.currentThread().getPriority()
+    );
+  }
+
+  public static void main(String args[]) {
+    thread4 m1 = new thread4();
+    thread4 m2 = new thread4();
+    m1.setPriority(Thread.MIN_PRIORITY);
+    m2.setPriority(Thread.MAX_PRIORITY);
+    m1.start();
+    m2.start();
+  }
+}
